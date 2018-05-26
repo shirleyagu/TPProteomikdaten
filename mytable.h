de98c65ee235725e7a6ef75529a84f872045c38c
Meowi <39214562+Meowi@users.noreply.github.com>
2018-05-26 14:01:58 +0200

    Create mytable.h

---
+++ mytable.h
@@ -0,0 +1,30 @@
+#ifndef MYTABLE_H
+#define MYTABLE_H
+#include<QAbstractTableModel>
+
+const int COLS= 12;
+const int ROWS= 6;
+
+
+class MyModel : public QAbstractTableModel {
+    Q_OBJECT
+   public:
+    //different function declarations to set data,column headers and tablesize
+    MyModel(QObject *parent);
+    int rowCount(const QModelIndex &parent=QModelIndex()) const override ;
+    int columnCount(const QModelIndex &parent=QModelIndex()) const override ;
+    QVariant data(const QModelIndex &index, int role=Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+
+    bool setData(const QModelIndex & index, const QVariant & value, int role = Qt::EditRole) override;
+    Qt::ItemFlags flags(const QModelIndex & index) const override ;
+
+
+
+};
+
+
+
+#endif // MYTABLE_H
+
+
