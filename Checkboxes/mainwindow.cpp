b74f7476a5063aad295ed3301193b71b3ab758a4
Meowi <39214562+Meowi@users.noreply.github.com>
2018-06-03 18:30:25 +0200

    Update mainwindow.cpp

--- Checkboxes/mainwindow.cpp
+++ Checkboxes/mainwindow.cpp
@@ -9,28 +9,30 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QStandardItemModel *model = new QStandardItemModel(5,12,this); //Erstellt eine Tabelle mit 5 Zeilen und 12 Spalten
+    const int rows = 5;
+    const int columns = 7;
+    const int check = 6;
+        
+    // Stringlisten für die Überschriften der Tabellen
+    QStringList Header1 = {"#","...","pi","Accession","Description","Chr","Coverage","#Peptides","#Spectra","MS2 Quant","MW","Confidence","Check"};
+    QStringList Header2 = {"#","...","Sequence","Start","#Spectra","Confidence","Check"};
+    
+     //Constructor der Tabelle
+    QStandardItemModel *model = new QStandardItemModel(rows,columns,this); //Erstellt eine Tabelle mit 5 Zeilen und 12 Spalten
+    for (int i= 0; i < columns; i++){                                                       //Einfügen der Header
+        model->setHorizontalHeaderItem(i, new QStandardItem(QString (Header2.at(i))));
+    }
 
-    model->setHorizontalHeaderItem(0, new QStandardItem(QString("Number")));        //Legt die Spaltenüberschriften Fest wie auf dem ÜB vorgegeben
-    model->setHorizontalHeaderItem(1, new QStandardItem(QString("Stern")));
-    model->setHorizontalHeaderItem(2, new QStandardItem(QString("PI")));
-    model->setHorizontalHeaderItem(3, new QStandardItem(QString("Accession")));
-    model->setHorizontalHeaderItem(4, new QStandardItem(QString("Description")));
-    model->setHorizontalHeaderItem(5, new QStandardItem(QString("Chr")));
-    model->setHorizontalHeaderItem(6, new QStandardItem(QString("Coverage")));
-    model->setHorizontalHeaderItem(7, new QStandardItem(QString("#Peptides")));
-    model->setHorizontalHeaderItem(8, new QStandardItem(QString("#Spectra")));
-    model->setHorizontalHeaderItem(9, new QStandardItem(QString("Quant")));
-    model->setHorizontalHeaderItem(10, new QStandardItem(QString("MW")));
-    model->setHorizontalHeaderItem(11, new QStandardItem(QString("Confidence")));
-    model->setHorizontalHeaderItem(12, new QStandardItem(QString("Check")));
-    for (int i=0; i< 5;i++){
+    for (int i=0; i< rows;i++){                                                              //Einfügen der Checkboxen
     QStandardItem* item0 = new QStandardItem(true);
     item0->setCheckable(true);
     item0->setCheckState(Qt::Checked);
-    model->setItem(i, 12, item0);
+    model->setItem(i, check, item0);
     }
+
+    ui->tableView->setEditTriggers(QAbstractItemView::DoubleClicked);                          
     ui->tableView->setModel(model);
+
 }
 
 MainWindow::~MainWindow()
