3ee2feb1d0b80402322616e049bc990a4c451319
JoergGamerdinger <33460322+JoergGamerdinger@users.noreply.github.com>
2018-06-04 07:58:37 +0200

    Delete main.cpp

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <QtCore/QCoreApplication>
-#include <QDebug>
-
-int main(int argc, char *argv[]){    //Deklerierung der Main-Methode
-qDebug()<< "Hello World!" <<endl;    //gibt Hellow World auf der Konsole aus...
-}
